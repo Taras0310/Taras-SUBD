e38f21463233f72f0d55a2bbab9f0c4e4b07e678
TarasKN-213 <65928868+TarasKN-213@users.noreply.github.com>
2020-12-15 16:54:29 +0200

    lab4-6

---
+++ Lab5.sql
@@ -0,0 +1,14 @@
+﻿USE [ChampionsLegaueDb]
+GO
+
+
+SELECT * FROM Команда2
+ UNION SELECT * FROM Команда3
+
+ SELECT * FROM Команда2
+ WHERE Команда2.[Код]  IN (SELECT [Код] FROM Команда3);
+
+ SELECT * FROM Команда2
+ WHERE Команда2.[Код] NOT IN (SELECT [Код] FROM Команда3);
+
+ SELECT * FROM Команда2, Команда3;
