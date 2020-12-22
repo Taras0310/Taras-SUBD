d32b126e8561a944649f66a243cdf00920fffc00
TarasKN-213 <65928868+TarasKN-213@users.noreply.github.com>
2020-12-22 17:32:04 +0200

    lab7-9

---
+++ Lab9.sql
@@ -0,0 +1,19 @@
+﻿USE [ChampionsLegaueDb]
+GO
+Select Avg(Місткість)
+From Стадіони
+Where Країна = 'Англія'
+
+SELECT  Група, COUNT(Код) as Кількість from Команда
+GROUP BY Команда.Група;
+
+
+Select SUM(Місткість)
+From Стадіони
+Where Країна = 'Англія'
+
+SELECT  YEAR(getdate())- AVG(DISTINCT [Рік заснування]) as Середній_Вік_Команди from Команда;
+
+Select MIN(Місткість)
+From Стадіони
+
