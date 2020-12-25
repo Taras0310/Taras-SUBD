de98f1f3bd70507315845f7b177d32f123c705aa
TarasKN-213 <65928868+TarasKN-213@users.noreply.github.com>
2020-12-25 22:03:17 +0200

    Lab13

---
+++ Lab13.sql
@@ -0,0 +1,25 @@
+﻿USE [ChampionsLegaueDb]
+GO
+SET SHOWPLAN_ALL ON;
+GO
+SELECT *
+FROM Стадіони
+GO
+SET SHOWPLAN_ALL OFF;
+
+SET SHOWPLAN_ALL ON;
+GO
+SELECT [Дата матча], [Код команди господарів],[Код команди гостей], [Забиті голи господарями], [Забиті гостями], Назва,[Головний суддя]
+FROM (dbo.Матч INNER JOIN dbo.Стадіони ON Матч.Стадіон =
+Стадіони.Стадіон)
+INNER JOIN dbo.Судді ON Судді.Бригада = Матч.[Суддівська бригада]
+GO
+SET SHOWPLAN_ALL OFF;
+
+SET SHOWPLAN_TEXT ON;
+GO
+SELECT *
+FROM Стадіони
+GO
+SET SHOWPLAN_TEXT OFF;
+
